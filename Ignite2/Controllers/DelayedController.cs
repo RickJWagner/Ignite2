3d036184cb93d17a2485396687d02fc10f1d367f
RickJWagner <RickJWag@yahoo.com>
2017-09-02 19:37:16 -0700

    Add new controller.

---
+++ Ignite2/Controllers/DelayedController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ignite2.Controllers
+{
+
+   public class DelayedController : Controller
+    {
+
+        [Route("")]
+        [Route("Delayed")]
+        [Route("Delayed/Index")]
+        public String MyIndex()
+        {
+            return "hey";
+        }
+
+
+
+
+    }
+}
